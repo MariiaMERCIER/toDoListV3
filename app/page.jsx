576ebd63df83b375700aa851ece554906dd90d90
Mariia IVANCHENKO <mariia.mercier@outlook.fr>
2023-01-15 21:53:23 +0100

    fetch

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,7 @@ import Toolbar from "../components/ToolBar";
 
 async function getData() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`, {
-      cache: "no-store",
-    });
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`);
 
     return res.json();
   } catch (error) {
