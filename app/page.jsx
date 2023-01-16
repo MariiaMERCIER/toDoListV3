44f6cc7f0c1e574483ee8603b272c2c3f3e27797
Mariia IVANCHENKO <mariia.mercier@outlook.fr>
2023-01-16 10:56:22 +0100

    theme

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,12 @@
-import { ThemeProvider } from "../context/ThemeProvider";
+// import { ThemeProvider } from "../context/ThemeProvider";
 
 import Toolbar from "../components/ToolBar";
 
 async function getData() {
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`
-      //  {
-      //   cache: "no-store",
-      // }
-    );
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`, {
+      cache: "no-store",
+    });
 
     return res.json();
   } catch (error) {
@@ -21,10 +18,10 @@ export default async function Home() {
   const data = await getData();
 
   return (
-    <ThemeProvider>
-      <main>
-        <Toolbar data={data} />
-      </main>{" "}
-    </ThemeProvider>
+    // <ThemeProvider>
+    <main>
+      <Toolbar data={data} />
+    </main>
+    // </ThemeProvider>
   );
 }
